8bbcad22a5293e71c0c0636a8869128d502780f8
Nikhil Bonte <nikhil.bonte@atlan.com>
2025-04-01 22:37:20 +0530

    POC Add time outs for Cassandra connection

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/CassandraConnector.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/CassandraConnector.java
@@ -1,6 +1,8 @@
 package org.apache.atlas.repository.store.graph.v2;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
+import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.cql.BoundStatement;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
@@ -15,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
+import java.time.Duration;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -47,6 +50,18 @@ public class CassandraConnector {
 
             cassSession = CqlSession.builder()
                     .addContactPoint(new InetSocketAddress(hostname, 9042))
+                    .withConfigLoader(
+                            DriverConfigLoader.programmaticBuilder()
+                                    .withDuration(DefaultDriverOption.CONNECTION_INIT_QUERY_TIMEOUT, Duration.ofSeconds(10))
+                                    .withDuration(DefaultDriverOption.CONNECTION_CONNECT_TIMEOUT, Duration.ofSeconds(15))
+                                    // Control timeout for requests
+                                    .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(15))
+                                    // Control timeout for schema agreement
+                                    .withDuration(DefaultDriverOption.CONTROL_CONNECTION_AGREEMENT_TIMEOUT, Duration.ofSeconds(20))
+                                    // More specific timeouts for different query types
+                                    .withDuration(DefaultDriverOption.REQUEST_TRACE_INTERVAL, Duration.ofMillis(500))
+                                    .withDuration(DefaultDriverOption.REQUEST_TRACE_ATTEMPTS, Duration.ofSeconds(20))
+                                    .build())
                     .withLocalDatacenter("datacenter1")
                     .withKeyspace(keyspace)
                     .build();
