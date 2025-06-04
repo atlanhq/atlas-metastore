da94b78c3ec8aab822bc3b2f2246d61f3768f1a1
Aarshi <aarshi.arora@atlan.com>
2025-06-04 17:22:47 +0530

    stop using graphIndexer

--- repository/src/main/java/org/apache/atlas/discovery/EntityDiscoveryService.java
+++ repository/src/main/java/org/apache/atlas/discovery/EntityDiscoveryService.java
@@ -78,6 +78,8 @@ import org.springframework.stereotype.Component;
 import javax.inject.Inject;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -85,6 +87,7 @@ import static org.apache.atlas.AtlasErrorCode.*;
 import static org.apache.atlas.SortOrder.ASCENDING;
 import static org.apache.atlas.model.instance.AtlasEntity.Status.ACTIVE;
 import static org.apache.atlas.model.instance.AtlasEntity.Status.DELETED;
+import static org.apache.atlas.model.typedef.AtlasBaseTypeDef.*;
 import static org.apache.atlas.repository.Constants.*;
 import static org.apache.atlas.repository.graph.GraphHelper.getAllTagNames;
 import static org.apache.atlas.repository.graph.GraphHelper.parseLabelsString;
@@ -1200,7 +1203,7 @@ public class EntityDiscoveryService implements AtlasDiscoveryService {
                     Class<?> clazz;
                     switch (attrType.getTypeCategory()) {
                         case PRIMITIVE:
-                            clazz = this.indexer.getPrimitiveClass(attrType.getTypeName());
+                            clazz = getPrimitiveClass(attrType.getTypeName());
                             break;
                         case STRUCT:
                             clazz = Map.class; // Structs are Map<String, Object>
@@ -1235,6 +1238,35 @@ public class EntityDiscoveryService implements AtlasDiscoveryService {
         return filteredMap;
     }
 
+    public Class getPrimitiveClass(String attribTypeName) {
+        String attributeTypeName = attribTypeName.toLowerCase();
+
+        switch (attributeTypeName) {
+            case ATLAS_TYPE_BOOLEAN:
+                return Boolean.class;
+            case ATLAS_TYPE_BYTE:
+                return Byte.class;
+            case ATLAS_TYPE_SHORT:
+                return Short.class;
+            case ATLAS_TYPE_INT:
+                return Integer.class;
+            case ATLAS_TYPE_LONG:
+            case ATLAS_TYPE_DATE:
+                return Long.class;
+            case ATLAS_TYPE_FLOAT:
+                return Float.class;
+            case ATLAS_TYPE_DOUBLE:
+                return Double.class;
+            case ATLAS_TYPE_BIGINTEGER:
+                return BigInteger.class;
+            case ATLAS_TYPE_BIGDECIMAL:
+                return BigDecimal.class;
+            case ATLAS_TYPE_STRING:
+                return String.class;
+        }
+
+        throw new IllegalArgumentException(String.format("Unknown primitive typename %s", attribTypeName));
+    }
     private void prepareSearchResultV2(AtlasSearchResult ret, DirectIndexQueryResult indexQueryResult, Set<String> resultAttributes, boolean fetchCollapsedResults) throws AtlasBaseException {
         AtlasPerfMetrics.MetricRecorder metricRecorder = RequestContext.get().startMetricRecord("prepareSearchResultV2");
         SearchParams searchParams = ret.getSearchParameters();
@@ -1393,7 +1425,7 @@ public class EntityDiscoveryService implements AtlasDiscoveryService {
                                 if (resultAttributes.contains(fqAttributeName)) {
                                     Object attributeValue;
                                     if (atlasType.getTypeCategory().equals(TypeCategory.PRIMITIVE)) {
-                                        attributeValue = vertex.getProperty(attributeTypeName, this.indexer.getPrimitiveClass(atlasType.getTypeName()));
+                                        attributeValue = vertex.getProperty(attributeTypeName, getPrimitiveClass(atlasType.getTypeName()));
                                     } else {
                                         attributeValue = vertex.getProperty(attributeTypeName, Object.class);
                                     }
