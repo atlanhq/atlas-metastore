3a6cc1d0d2ff862fccb7fa7421bc4f91a6ffaa69
ankitpatnaik-atlan <ankit.patnaik@atlan.com>
2025-06-04 17:04:13 +0530

    mesh-407: fetch using indexsearch

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/bulkimport/MigrationImport.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/bulkimport/MigrationImport.java
@@ -19,6 +19,7 @@
 package org.apache.atlas.repository.store.graph.v2.bulkimport;
 
 import org.apache.atlas.AtlasErrorCode;
+import org.apache.atlas.AtlasException;
 import org.apache.atlas.exception.AtlasBaseException;
 import org.apache.atlas.model.impexp.AtlasImportRequest;
 import org.apache.atlas.model.impexp.AtlasImportResult;
@@ -60,7 +61,7 @@ public class MigrationImport extends ImportStrategy {
         LOG.info("MigrationImport: Using bulkLoading...");
     }
 
-    public EntityMutationResponse run(EntityImportStream entityStream, AtlasImportResult importResult) throws AtlasBaseException {
+    public EntityMutationResponse run(EntityImportStream entityStream, AtlasImportResult importResult) throws AtlasBaseException, AtlasException {
         if (entityStream == null || !entityStream.hasNext()) {
             throw new AtlasBaseException(AtlasErrorCode.INVALID_PARAMETERS, "no entities to create/update.");
         }
@@ -98,7 +99,7 @@ public class MigrationImport extends ImportStrategy {
     }
 
     private EntityCreationManager createEntityCreationManager(AtlasImportResult importResult,
-                                                              DataMigrationStatusService dataMigrationStatusService) {
+                                                              DataMigrationStatusService dataMigrationStatusService) throws AtlasException {
         AtlasGraph graphBulk = graphProvider.getBulkLoading();
 
         EntityGraphRetriever entityGraphRetriever = this.entityGraphRetriever;
@@ -124,7 +125,7 @@ public class MigrationImport extends ImportStrategy {
         return ret;
     }
 
-    private AtlasEntityStoreV2 createEntityStore(AtlasGraph graph, AtlasTypeRegistry typeRegistry) {
+    private AtlasEntityStoreV2 createEntityStore(AtlasGraph graph, AtlasTypeRegistry typeRegistry) throws AtlasException {
         FullTextMapperV2Nop fullTextMapperV2 = new FullTextMapperV2Nop();
         DeleteHandlerDelegate deleteDelegate = new DeleteHandlerDelegate(graph, typeRegistry, null, null);
         RestoreHandlerV1 restoreHandlerV1 = new RestoreHandlerV1(graph, typeRegistry, entityGraphRetriever);
