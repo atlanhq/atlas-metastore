ddeb2c1a812a3f0fa914e88c4a77e703111c04d7
Krishnanunni M <krishnanunni.m@atlan.com>
2025-10-13 17:24:02 +0530

    testing with maven

--- common/src/main/java/org/apache/atlas/service/redis/RedisServiceImpl.java
+++ common/src/main/java/org/apache/atlas/service/redis/RedisServiceImpl.java
@@ -1,6 +1,7 @@
 package org.apache.atlas.service.redis;
 
 import org.apache.atlas.AtlasException;
+import org.apache.atlas.annotation.ConditionalOnAtlasProperty;
 import org.redisson.Redisson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 
 @Component
+@ConditionalOnAtlasProperty(property = "atlas.redis.service.impl")
 @Profile("!local")
 public class RedisServiceImpl extends AbstractRedisService{
 
