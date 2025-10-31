a0ba5f09c92390a7351021be1a6e5abb726b590e
sriram-atlan <sriram.aravamuthan@atlan.com>
2025-10-31 15:09:00 +0530

    MLH-1523 move it inside the internalAttributes null safe block

--- webapp/src/main/java/org/apache/atlas/notification/EntityNotificationListenerV2.java
+++ webapp/src/main/java/org/apache/atlas/notification/EntityNotificationListenerV2.java
@@ -314,18 +314,17 @@ public class EntityNotificationListenerV2 implements EntityChangeListenerV2 {
             Map<String, Object> allInternalAttributesMap = context.getAllInternalAttributesMap().get(entity.getGuid());
             if (MapUtils.isNotEmpty(allInternalAttributesMap)) {
                 ret.setInternalAttributes(allInternalAttributesMap);
-            }
-
-            // fill all classifications related attrs. They are no longer part of vertex attributes
-            if (CollectionUtils.isNotEmpty(context.getESDeferredOperations())) {
-                List<ESDeferredOperation> esDefferredOperations = context.getESDeferredOperations();
-                if (CollectionUtils.isNotEmpty(esDefferredOperations)) {
-                    for (ESDeferredOperation esDeferredOperation : esDefferredOperations) {
-                        Long vertexId = LongEncoding.decode(entity.getDocId());
-                        if (Long.parseLong(esDeferredOperation.getEntityId()) == vertexId) {
-                            Map<String, Object> classificationInternalAttributes = esDeferredOperation.getPayload().get(String.valueOf(vertexId));
-                            if (MapUtils.isNotEmpty(classificationInternalAttributes)) {
-                                ret.getInternalAttributes().putAll(classificationInternalAttributes);
+                // fill all classifications related attrs. They are no longer part of vertex attributes
+                if (CollectionUtils.isNotEmpty(context.getESDeferredOperations())) {
+                    List<ESDeferredOperation> esDefferredOperations = context.getESDeferredOperations();
+                    if (CollectionUtils.isNotEmpty(esDefferredOperations)) {
+                        for (ESDeferredOperation esDeferredOperation : esDefferredOperations) {
+                            Long vertexId = LongEncoding.decode(entity.getDocId());
+                            if (Long.parseLong(esDeferredOperation.getEntityId()) == vertexId) {
+                                Map<String, Object> classificationInternalAttributes = esDeferredOperation.getPayload().get(String.valueOf(vertexId));
+                                if (MapUtils.isNotEmpty(classificationInternalAttributes)) {
+                                    ret.getInternalAttributes().putAll(classificationInternalAttributes);
+                                }
                             }
                         }
                     }
