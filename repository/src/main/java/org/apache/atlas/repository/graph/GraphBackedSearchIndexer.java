a5341dc1ce59f0a10671df5375095a84b5bf9d54
sriram-atlan <sriram.aravamuthan@atlan.com>
2025-11-27 23:37:31 +0530

    MLH-1799 add start up metrics for typedef store, graph and task registry

--- repository/src/main/java/org/apache/atlas/repository/graph/GraphBackedSearchIndexer.java
+++ repository/src/main/java/org/apache/atlas/repository/graph/GraphBackedSearchIndexer.java
@@ -20,6 +20,7 @@ package org.apache.atlas.repository.graph;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
+import io.micrometer.core.instrument.Timer;
 import org.apache.atlas.ApplicationProperties;
 import org.apache.atlas.AtlasErrorCode;
 import org.apache.atlas.AtlasException;
@@ -29,6 +30,7 @@ import org.apache.atlas.ha.HAConfiguration;
 import org.apache.atlas.listener.ActiveStateChangeHandler;
 import org.apache.atlas.listener.ChangedTypeDefs;
 import org.apache.atlas.listener.TypeDefChangeListener;
+import org.apache.atlas.service.metrics.MetricUtils;
 import org.apache.atlas.model.TypeCategory;
 import org.apache.atlas.model.typedef.AtlasBaseTypeDef;
 import org.apache.atlas.model.typedef.AtlasEntityDef;
@@ -295,6 +297,7 @@ public class GraphBackedSearchIndexer implements SearchIndexer, ActiveStateChang
      * Initializes the indices for the graph - create indices for Global AtlasVertex and AtlasEdge Keys
      */
     private void initialize(AtlasGraph graph) throws RepositoryException, IndexException {
+        Timer.Sample sample = Timer.start(MetricUtils.getMeterRegistry());
         AtlasGraphManagement management = graph.getManagementSystem();
 
         try {
@@ -449,6 +452,10 @@ public class GraphBackedSearchIndexer implements SearchIndexer, ActiveStateChang
 
             rollback(management);
             throw new RepositoryException(t);
+        } finally {
+            sample.stop(Timer.builder("atlas.startup.graph.index.init.duration")
+                    .description("Time taken to initialize graph indices during Atlas startup")
+                    .register(MetricUtils.getMeterRegistry()));
         }
     }
 
