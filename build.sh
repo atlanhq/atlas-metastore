d8782d9f6b025176c22901fc15b3b670bfcd67a7
Suman Das <59254445+sumandas0@users.noreply.github.com>
2026-02-12 03:24:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,9 +21,10 @@ mkdir -p ~/.m2/repository/org/keycloak
 wget  https://atlan-public.s3.eu-west-1.amazonaws.com/artifact/keycloak-15.0.2.1.zip
 unzip -o keycloak-15.0.2.1.zip -d ~/.m2/repository/org
 
-echo "Maven Building (parallel mode)"
+echo "Maven Building"
 
-mvn -T 1C clean -U -Dmaven.test.skip -DskipTests -Drat.skip=true -DskipOverlay -DskipEnunciate=true install package -Pdist
+mvn clean -U -Dmaven.test.skip -DskipTests -Drat.skip=true -DskipOverlay -DskipEnunciate=true install package -Pdist
+
 echo "[DEBUG listing distro/target"
 ls distro/target
 
