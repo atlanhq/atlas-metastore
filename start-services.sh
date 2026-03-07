5968119b916b47c49623e91f33026e470d30ec7f
muniatl <muniraju.pulipalyam@atlan.com>
2026-03-07 11:18:38 +0530

    run atlas_start in background to force atlas in foreground

--- start-services.sh
+++ start-services.sh
@@ -3,7 +3,9 @@
 echo "Starting Apache Atlas..."
 
 cd /opt/apache-atlas/bin
-./atlas_start.py
+./atlas_start.py &
+echo "Starting Atlas waiting for 10 seconds for process initialization..."
+sleep 10
 
 echo "Starting Context Engine..."
 
