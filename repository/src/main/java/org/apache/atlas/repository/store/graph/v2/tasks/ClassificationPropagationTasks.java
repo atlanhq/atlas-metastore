8582fa36e0ab34fa820dab0a228156f935fca80e
salman_atlan <salman.khurshid@atlan.com>
2026-03-25 14:06:19 +0530

    fix: (MS-609) Improving Task Lifecycl Management in Apps Team Workflows

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/tasks/ClassificationPropagationTasks.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/tasks/ClassificationPropagationTasks.java
@@ -22,6 +22,7 @@ import org.apache.atlas.exception.AtlasBaseException;
 import org.apache.atlas.repository.metrics.TaskMetricsService;
 import org.apache.atlas.model.instance.AtlasRelationship;
 import org.apache.atlas.model.tasks.AtlasTask;
+import org.apache.atlas.notification.task.TaskNotificationSender;
 import org.apache.atlas.repository.graphdb.AtlasGraph;
 import org.apache.atlas.repository.store.graph.AtlasRelationshipStore;
 import org.apache.atlas.repository.store.graph.v1.DeleteHandlerDelegate;
@@ -36,8 +37,8 @@ public class ClassificationPropagationTasks {
     private static final Logger LOG = LoggerFactory.getLogger(ClassificationPropagationTasks.class);
 
     public static class Add extends ClassificationTask {
-        public Add(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService) {
-            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService);
+        public Add(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService, TaskNotificationSender taskNotificationSender) {
+            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService, taskNotificationSender);
         }
 
         @Override
@@ -66,8 +67,8 @@ public class ClassificationPropagationTasks {
     }
 
     public static class UpdateText extends ClassificationTask {
-        public UpdateText(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService) {
-            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService);
+        public UpdateText(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService, TaskNotificationSender taskNotificationSender) {
+            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService, taskNotificationSender);
         }
 
         @Override
@@ -91,8 +92,8 @@ public class ClassificationPropagationTasks {
     }
 
     public static class Delete extends ClassificationTask {
-        public Delete(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService) {
-            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService);
+        public Delete(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService, TaskNotificationSender taskNotificationSender) {
+            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService, taskNotificationSender);
         }
 
         @Override
@@ -118,8 +119,8 @@ public class ClassificationPropagationTasks {
     }
 
     public static class RefreshPropagation extends ClassificationTask {
-        public RefreshPropagation(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService) {
-            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService);
+        public RefreshPropagation(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService, TaskNotificationSender taskNotificationSender) {
+            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService, taskNotificationSender);
         }
 
         @Override
@@ -144,8 +145,8 @@ public class ClassificationPropagationTasks {
     }
 
     public static class UpdateRelationship extends ClassificationTask {
-        public UpdateRelationship(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService) {
-            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService);
+        public UpdateRelationship(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService, TaskNotificationSender taskNotificationSender) {
+            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService, taskNotificationSender);
         }
 
         @Override
@@ -158,8 +159,8 @@ public class ClassificationPropagationTasks {
     }
 
     public static class CleanUpClassificationPropagation extends ClassificationTask {
-        public CleanUpClassificationPropagation(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService) {
-            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService);
+        public CleanUpClassificationPropagation(AtlasTask task, AtlasGraph graph, EntityGraphMapper entityGraphMapper, DeleteHandlerDelegate deleteDelegate, AtlasRelationshipStore relationshipStore, TaskMetricsService taskMetricsService, TaskNotificationSender taskNotificationSender) {
+            super(task, graph, entityGraphMapper, deleteDelegate, relationshipStore, taskMetricsService, taskNotificationSender);
         }
 
         @Override
