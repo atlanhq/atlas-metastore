9b129634868bdfdcbba6b18873a52127c27c6f69
sriram-atlan <sriram.aravamuthan@atlan.com>
2026-03-26 21:36:47 +0530

    Revert "MS-885: Migrate embedded server from Jetty 9.4 to Undertow for improv…" (#6409)
    This reverts commit b9f692ba97c509b778f27390b02d26faf36b29d3.

--- webapp/src/main/java/org/apache/atlas/web/service/SecureEmbeddedServer.java
+++ webapp/src/main/java/org/apache/atlas/web/service/SecureEmbeddedServer.java
@@ -18,41 +18,64 @@
 
 package org.apache.atlas.web.service;
 
-import io.undertow.Undertow;
 import org.apache.atlas.ApplicationProperties;
+import org.apache.atlas.AtlasConfiguration;
 import org.apache.atlas.AtlasException;
+import org.apache.atlas.security.SecurityUtil;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.security.alias.CredentialProvider;
 import org.apache.hadoop.security.alias.CredentialProviderFactory;
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
+import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.util.List;
 
-import static org.apache.atlas.security.SecurityProperties.*;
+import static org.apache.atlas.security.SecurityProperties.ATLAS_SSL_EXCLUDE_CIPHER_SUITES;
+import static org.apache.atlas.security.SecurityProperties.CERT_STORES_CREDENTIAL_PROVIDER_PATH;
+import static org.apache.atlas.security.SecurityProperties.CLIENT_AUTH_KEY;
+import static org.apache.atlas.security.SecurityProperties.DEFATULT_TRUSTORE_FILE_LOCATION;
+import static org.apache.atlas.security.SecurityProperties.DEFAULT_CIPHER_SUITES;
+import static org.apache.atlas.security.SecurityProperties.DEFAULT_KEYSTORE_FILE_LOCATION;
+import static org.apache.atlas.security.SecurityProperties.KEYSTORE_FILE_KEY;
+import static org.apache.atlas.security.SecurityProperties.KEYSTORE_PASSWORD_KEY;
+import static org.apache.atlas.security.SecurityProperties.SERVER_CERT_PASSWORD_KEY;
+import static org.apache.atlas.security.SecurityProperties.TRUSTSTORE_FILE_KEY;
+import static org.apache.atlas.security.SecurityProperties.TRUSTSTORE_PASSWORD_KEY;
+import static org.apache.atlas.security.SecurityProperties.ATLAS_SSL_EXCLUDE_PROTOCOLS;
+import static org.apache.atlas.security.SecurityProperties.DEFAULT_EXCLUDE_PROTOCOLS;
+import static org.apache.atlas.security.SecurityProperties.KEYSTORE_TYPE;
+import static org.apache.atlas.security.SecurityProperties.TRUSTSTORE_TYPE;
 import static org.apache.atlas.security.SecurityUtil.getPassword;
 
+
 /**
- * Undertow-based HTTPS embedded server with client certificate support.
- * Replaces the Jetty-based SecureEmbeddedServer.
+ * This is a jetty server which requires client auth via certificates.
  */
 public class SecureEmbeddedServer extends EmbeddedServer {
 
@@ -64,22 +87,74 @@ public class SecureEmbeddedServer extends EmbeddedServer {
     public static final String ATLAS_TLS_KEYMANAGER_DEFAULT_ALGO_TYPE = KeyManagerFactory.getDefaultAlgorithm();
     public static final String ATLAS_TLS_TRUSTMANAGER_DEFAULT_ALGO_TYPE = TrustManagerFactory.getDefaultAlgorithm();
 
+
+
     public SecureEmbeddedServer(String host, int port, String path) throws IOException {
         super(host, port, path);
     }
 
     @Override
-    protected void configureListener(Undertow.Builder builder, String host, int port) {
+    protected Connector getConnector(String host, int port) throws IOException {
+        org.apache.commons.configuration.Configuration config = getConfiguration();
+
         SSLContext sslContext = getSSLContext();
         if (sslContext != null) {
-            builder.addHttpsListener(port, host, sslContext);
-            LOG.info("Undertow HTTPS listener configured on {}:{}", host, port);
-        } else {
-            LOG.warn("SSLContext is null, falling back to HTTP listener");
-            builder.addHttpListener(port, host);
+            SSLContext.setDefault(sslContext);
         }
+
+        SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
+        sslContextFactory.setKeyStoreType(config.getString(KEYSTORE_TYPE , ATLAS_KEYSTORE_FILE_TYPE_DEFAULT));
+        sslContextFactory.setKeyStorePath(config.getString(KEYSTORE_FILE_KEY,
+                System.getProperty(KEYSTORE_FILE_KEY, DEFAULT_KEYSTORE_FILE_LOCATION)));
+        sslContextFactory.setKeyStorePassword(getPassword(config, KEYSTORE_PASSWORD_KEY));
+        sslContextFactory.setKeyManagerPassword(getPassword(config, SERVER_CERT_PASSWORD_KEY));
+        sslContextFactory.setTrustStoreType(config.getString(TRUSTSTORE_TYPE , ATLAS_TRUSTSTORE_FILE_TYPE_DEFAULT));
+        sslContextFactory.setTrustStorePath(config.getString(TRUSTSTORE_FILE_KEY,
+                System.getProperty(TRUSTSTORE_FILE_KEY, DEFATULT_TRUSTORE_FILE_LOCATION)));
+        sslContextFactory.setTrustStorePassword(getPassword(config, TRUSTSTORE_PASSWORD_KEY));
+        sslContextFactory.setWantClientAuth(config.getBoolean(CLIENT_AUTH_KEY, Boolean.getBoolean(CLIENT_AUTH_KEY)));
+
+        List<Object> cipherList = config.getList(ATLAS_SSL_EXCLUDE_CIPHER_SUITES, DEFAULT_CIPHER_SUITES);
+        sslContextFactory.setExcludeCipherSuites(cipherList.toArray(new String[cipherList.size()]));
+        sslContextFactory.setRenegotiationAllowed(false);
+
+        String[] excludedProtocols = config.containsKey(ATLAS_SSL_EXCLUDE_PROTOCOLS) ?
+                config.getStringArray(ATLAS_SSL_EXCLUDE_PROTOCOLS) : DEFAULT_EXCLUDE_PROTOCOLS;
+        if (excludedProtocols != null && excludedProtocols.length > 0) {
+            sslContextFactory.addExcludeProtocols(excludedProtocols);
+        }
+
+        // SSL HTTP Configuration
+        // HTTP Configuration
+        HttpConfiguration http_config = new HttpConfiguration();
+        http_config.setSecureScheme("https");
+        final int bufferSize = AtlasConfiguration.WEBSERVER_REQUEST_BUFFER_SIZE.getInt();
+        http_config.setSecurePort(port);
+        http_config.setRequestHeaderSize(bufferSize);
+        http_config.setResponseHeaderSize(bufferSize);
+        http_config.setSendServerVersion(false);
+        http_config.setSendDateHeader(false);
+
+        HttpConfiguration https_config = new HttpConfiguration(http_config);
+        https_config.addCustomizer(new SecureRequestCustomizer());
+        https_config.setSendServerVersion(false);
+
+        // SSL Connector
+        ServerConnector sslConnector = new ServerConnector(server,
+            new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+            new HttpConnectionFactory(https_config));
+        sslConnector.setPort(port);
+        server.addConnector(sslConnector);
+
+        return sslConnector;
     }
 
+
+
+    /**
+     * Returns the application configuration.
+     * @return
+     */
     protected org.apache.commons.configuration.Configuration getConfiguration() {
         try {
             return ApplicationProperties.get();
@@ -88,6 +163,10 @@ public class SecureEmbeddedServer extends EmbeddedServer {
         }
     }
 
+    /**
+     * creates the SSLContext using the available keystore and truststore.
+     * @return
+     */
     private SSLContext getSSLContext() {
         KeyManager[] kmList = getKeyManagers();
         TrustManager[] tmList = getTrustManagers();
@@ -99,92 +178,135 @@ public class SecureEmbeddedServer extends EmbeddedServer {
             } catch (NoSuchAlgorithmException e) {
                 LOG.error("SSL algorithm is not available in the environment. Reason: " + e.toString());
             } catch (KeyManagementException e) {
-                LOG.error("Unable to initialize the SSLContext. Reason: " + e.toString());
+                LOG.error("Unable to initials the SSLContext. Reason: " + e.toString());
             }
         }
         return sslContext;
     }
 
+    /**
+     * Generating the KeyManager using the provided keystore.
+     * @return
+     */
     private KeyManager[] getKeyManagers() {
         KeyManager[] kmList = null;
         try {
+
             String keyStoreFile = getConfiguration().getString(KEYSTORE_FILE_KEY,
-                    System.getProperty(KEYSTORE_FILE_KEY, DEFAULT_KEYSTORE_FILE_LOCATION));
+              System.getProperty(KEYSTORE_FILE_KEY, DEFAULT_KEYSTORE_FILE_LOCATION));
             String keyStoreFilepwd = getPassword(getConfiguration(), KEYSTORE_PASSWORD_KEY);
 
             if (StringUtils.isNotEmpty(keyStoreFile) && StringUtils.isNotEmpty(keyStoreFilepwd)) {
                 InputStream in = null;
+
                 try {
                     in = getFileInputStream(keyStoreFile);
+
                     if (in != null) {
-                        KeyStore keyStore = KeyStore.getInstance(
-                                getConfiguration().getString(KEYSTORE_TYPE, ATLAS_KEYSTORE_FILE_TYPE_DEFAULT));
+                        KeyStore keyStore = KeyStore.getInstance(getConfiguration().getString(KEYSTORE_TYPE , ATLAS_KEYSTORE_FILE_TYPE_DEFAULT));
+
                         keyStore.load(in, keyStoreFilepwd.toCharArray());
+
                         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(ATLAS_TLS_KEYMANAGER_DEFAULT_ALGO_TYPE);
+
                         keyManagerFactory.init(keyStore, keyStoreFilepwd.toCharArray());
+
                         kmList = keyManagerFactory.getKeyManagers();
                     } else {
                         LOG.error("Unable to obtain keystore from file [" + keyStoreFile + "]");
                     }
-                } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException |
-                         FileNotFoundException | UnrecoverableKeyException e) {
-                    LOG.error("Error loading keystore", e);
+                } catch (KeyStoreException e) {
+                    LOG.error("Unable to obtain from KeyStore :" + e.getMessage(), e);
+                } catch (NoSuchAlgorithmException e) {
+                    LOG.error("SSL algorithm is NOT available in the environment", e);
+                } catch (CertificateException e) {
+                    LOG.error("Unable to obtain the requested certification ", e);
+                } catch (FileNotFoundException e) {
+                    LOG.error("Unable to find the necessary TLS Keystore Files", e);
+                } catch (IOException e) {
+                    LOG.error("Unable to read the necessary TLS Keystore Files", e);
+                } catch (UnrecoverableKeyException e) {
+                    LOG.error("Unable to recover the key from keystore", e);
                 } finally {
                     close(in, keyStoreFile);
                 }
             }
-        } catch (IOException exception) {
+
+        }catch (IOException exception) {
             LOG.error(exception.getMessage());
         }
         return kmList;
     }
 
+    /**
+     * Generating the TrustManager using the provided truststore
+     * @return
+     */
     private TrustManager[] getTrustManagers() {
         TrustManager[] tmList = null;
         try {
             String truststoreFile = getConfiguration().getString(TRUSTSTORE_FILE_KEY,
-                    System.getProperty(TRUSTSTORE_FILE_KEY, DEFATULT_TRUSTORE_FILE_LOCATION));
+              System.getProperty(TRUSTSTORE_FILE_KEY, DEFATULT_TRUSTORE_FILE_LOCATION));
             String trustStoreFilepwd = getPassword(getConfiguration(), TRUSTSTORE_PASSWORD_KEY);
 
             if (StringUtils.isNotEmpty(truststoreFile) && StringUtils.isNotEmpty(trustStoreFilepwd)) {
                 InputStream in = null;
+
                 try {
                     in = getFileInputStream(truststoreFile);
+
                     if (in != null) {
-                        KeyStore trustStore = KeyStore.getInstance(
-                                getConfiguration().getString(TRUSTSTORE_TYPE, ATLAS_TRUSTSTORE_FILE_TYPE_DEFAULT));
+                        KeyStore trustStore = KeyStore.getInstance(getConfiguration().getString(TRUSTSTORE_TYPE , ATLAS_TRUSTSTORE_FILE_TYPE_DEFAULT));
+
                         trustStore.load(in, trustStoreFilepwd.toCharArray());
+
                         TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(ATLAS_TLS_TRUSTMANAGER_DEFAULT_ALGO_TYPE);
+
                         trustManagerFactory.init(trustStore);
+
                         tmList = trustManagerFactory.getTrustManagers();
                     } else {
                         LOG.error("Unable to obtain truststore from file [" + truststoreFile + "]");
                     }
-                } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException |
-                         FileNotFoundException e) {
-                    LOG.error("Error loading truststore", e);
+                } catch (KeyStoreException e) {
+                    LOG.error("Unable to obtain from KeyStore", e);
+                } catch (NoSuchAlgorithmException e) {
+                    LOG.error("SSL algorithm is NOT available in the environment :" + e.getMessage(), e);
+                } catch (CertificateException e) {
+                    LOG.error("Unable to obtain the requested certification :" + e.getMessage(), e);
+                } catch (FileNotFoundException e) {
+                    LOG.error("Unable to find the necessary TLS TrustStore File:" + truststoreFile, e);
+                } catch (IOException e) {
+                    LOG.error("Unable to read the necessary TLS TrustStore Files :" + truststoreFile, e);
                 } finally {
                     close(in, truststoreFile);
                 }
             }
-        } catch (IOException exception) {
+
+        }catch (IOException exception) {
             LOG.error(exception.getMessage());
         }
         return tmList;
     }
 
     private InputStream getFileInputStream(String fileName) throws IOException {
+        InputStream in = null;
         if (StringUtils.isNotEmpty(fileName)) {
             File f = new File(fileName);
             if (f.exists()) {
-                return new FileInputStream(f);
+                in = new FileInputStream(f);
             } else {
-                return ClassLoader.getSystemResourceAsStream(fileName);
+                in = ClassLoader.getSystemResourceAsStream(fileName);
             }
         }
-        return null;
+        return in;
     }
 
+    /**
+     * Closing file-stream.
+     * @param str
+     * @param filename
+     */
     private void close(InputStream str, String filename) {
         if (str != null) {
             try {
