4b7f0d507fa540036604230a4fb8a133cc1070c6
Nikhil Bonte <nikhil.bonte@atlan.com>
2026-04-20 20:30:48 +0530

    MS-771 Enable tag propagation across Tableau relationships

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasRelationshipStoreV2.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasRelationshipStoreV2.java
@@ -119,6 +119,11 @@ public class AtlasRelationshipStoreV2 implements AtlasRelationshipStore {
         RELATIONSHIP_HARD_DELETE
     }
 
+    private static Set<String> EXCLUDE_FLIP_TAG_PROP_REL_TYPE_NAMES = new HashSet<String>() {{
+        add(REL_TABLEAU_PROJECT_PROJECTS);
+        add(REL_TABLEAU_DASHBOARDS_DASHBOARDS);
+    }};
+
     @Inject
     public AtlasRelationshipStoreV2(AtlasGraph graph, AtlasTypeRegistry typeRegistry, DeleteHandlerDelegate deleteDelegate,
                                     IAtlasEntityChangeNotifier entityChangeNotifier, EntityGraphRetriever entityRetriever) {
@@ -804,7 +809,7 @@ public class AtlasRelationshipStoreV2 implements AtlasRelationshipStore {
         // relationshipDef is defined as end1 (hive_db) and end2 (hive_table) and tagPropagation = ONE_TO_TWO
         // relationship edge exists from [hive_table --> hive_db]
         // swap the tagPropagation property for such cases.
-        if (fromVertexTypes.contains(endDef2.getType()) && toVertexTypes.contains(endDef1.getType())) {
+        if (!EXCLUDE_FLIP_TAG_PROP_REL_TYPE_NAMES.contains(relationship.getTypeName()) && fromVertexTypes.contains(endDef2.getType()) && toVertexTypes.contains(endDef1.getType())) {
             if (ret == ONE_TO_TWO) {
                 ret = TWO_TO_ONE;
             } else if (ret == TWO_TO_ONE) {
